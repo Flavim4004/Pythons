0e0c1a77c6faffd498ae3e329d684d7faeea4e38
Flavim4004 <215928528+Flavim4004@users.noreply.github.com>
2025-07-03 20:42:11 -0300

    Add files via upload

--- Exe1.py
+++ Exe1.py
@@ -1,18 +1,5 @@
-n1=int(input("Digite um numero fih! \n"))
-n2=int(input("Digite um numero fih! \n"))
-resp=int(input("Escolha um calculo fih! \n [1]Soma \n [2]Subtração \n [3]Multiplicação \n [4]Divisão \n"))
-match resp:
-    case 1:
-        soma=int(n1+n2)
-        print("A soma é " + str(soma))
-    case 2:
-        sub=int(n1-n2)
-        print("A subtração é " + str(sub))
-    case 3:
-        mult=int(n1*n2)
-        print("A multiplicação é " + str(mult))
-    case 4:
-        div=int(n1/n2)
-        print("A divisão é " + str(div))
-    case _:
-        print("Invalido fih")
+sal=float(input("Qual o seu salario fih? \n"))
+f=float(sal +(sal*1.33))
+a=float(sal+(sal*0.74))
+p=float(sal+(sal*1.45))
+print("Baseado no seu salario suas férias seriam " + str(f) + " seu bônus seria " + str(a) + " e caso você fosse promovida seria  " + str(p))
--- Exe2.py
+++ Exe2.py
@@ -1,23 +1,6 @@
-peso=float(input("Qual seu peso no planeta terra fih? \n "))
-p=int(input("Escolha um planeta para conversão de peso fih \n [1]Mercurio \n [2]Venus \n [3]Marte \n [4]Jupter \n [5]Saturno \n [6]Urano \n"))
-match p:
-    case 1:
-        m1=(peso+(peso*0.37))
-        print("Seu peso em Mercurio Seria " + str(m1))
-    case 2:
-        m2=(peso+(peso*0.88))
-        print("Seu peso em Venus seria " + str(m2))
-    case 3:
-        m3=(peso+(peso *0.38))
-        print("Seu peso em Marte seria " + str(m3))
-    case 4:
-        m4=(peso+(peso*2.64))
-        print("Seu peso em Jupter seria " + str(m4))
-    case 5:
-        m5=(peso+(peso*1.15))
-        print("Seu peso  em  Saturno seria " + str(m5))
-    case 6:
-        m6=(peso+(peso*1.17))
-        print("Seu peso em Urano seria "+ str(m6))
-    case _:
-        print("RESPOSTA INVALIDA FIH!")
+peso=float(input("Quanto voce pesa fih? \n"))
+dias=float(input("Quantos dias voce vai na academia fih? \n"))
+massa=float(dias*0.20)
+perda=float(dias*0.40)
+total=float(peso+massa-perda)
+print("Voce pessava " + str(peso) + " e perdeu " + str(perda) + " de massa gorda porem ganhou " + str(massa)+ " de massa muscular, pesando hoje " + str(total))
